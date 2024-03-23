5b8512d2c6b89c70185e2492e3736dc83d732dcb
Jongita <jurgitajonaityte@gmail.com>
2024-03-23 21:27:04 +0200

    Updated

--- js1/pirma.js
+++ js1/pirma.js
@@ -26,3 +26,23 @@ console.log("minTemp: " + sorted[sorted.length - 1], dataArray.indexOf(sorted[so
 
 // console.log(indexOfAll(dataArray, sorted[sorted.length - 1]));
 // console.log(indexOfAll(dataArray, sorted[0]));
+
+// // Failo nuskaitymas
+// const fs = require("fs");
+// const input = fs.readFileSync(process.argv[2]).toString().split(" ").map(Number);
+// console.log(typeof input[0]);
+// let minT = input[0];
+// let minD = 1;
+// let maxT = input[0];
+// let maxD = 1;
+// input.forEach((d, i) => {
+//     if (minT > d) {
+//         minT = d;
+//         minD = i + 1;
+//     }
+//     if (maxT < d) {
+//         maxT = d;
+//         maxD = i + 1;
+//     }
+// });
+// console.log(`Min: ${minT}, ${minD}  Max: ${maxT}, ${maxD}`);
